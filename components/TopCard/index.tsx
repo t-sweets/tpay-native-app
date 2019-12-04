be2b1b706725acdaaa1534514d4d8a92502cabee
sho <sskmy1024.y@gmail.com>
2019-12-04 20:34:22 +0900

    update topcard design

--- components/TopCard/index.tsx
+++ components/TopCard/index.tsx
@@ -19,11 +19,12 @@ function TopCard({ balance }: TopCardProps) {
       imageStyle={{ borderRadius: 10 }}
       source={require("../../assets/t-pay-card.png")}
     >
-      <Text style={styles.title}>{"あなたの残高"}</Text>
-      <View style={styles.balanceView}>
-        <Text style={styles.balanceText}>{balance}</Text>
-        <Text style={styles.balanceUnit}>{"円"}</Text>
-      </View>
+      <Title>{"あなたの残高"}</Title>
+      <BalanceText>
+        {balance}
+        <BalanceUnit>{"円"}</BalanceUnit>
+      </BalanceText>
+      <View />
     </CardView>
   );
 }
@@ -33,31 +34,26 @@ const CardView = styled.ImageBackground`
   height: ${Dimensions.get("screen").width * 0.55};
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 
   box-shadow: 0px 0px 10px #ccc;
 `;
 
-const styles = StyleSheet.create({
-  title: {
-    color: "gray",
-    fontSize: 22,
-    marginTop: 20
-  },
-  balanceView: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  balanceText: {
-    fontSize: 45,
-    fontWeight: "500",
-    lineHeight: 140,
-    margin: 2
-  },
-  balanceUnit: {
-    fontSize: 18,
-    margin: 2,
-    lineHeight: 135
-  }
-});
+const Title = styled.Text`
+  color: gray;
+  font-size: 20;
+  margin-top: 25;
+`;
+
+const BalanceText = styled.Text`
+  font-size: 45;
+  font-weight: 500;
+  margin-bottom: 35;
+`;
+
+const BalanceUnit = styled.Text`
+  font-size: 18;
+  margin: 2px;
+`;
 
 export default TopCard;
